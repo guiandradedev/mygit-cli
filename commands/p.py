ee69022df1c01cce283bed807e9f33d3531ff05c
Guilherme Andrade <guiandradeemail@gmail.com>
2025-04-18 00:02:04 -0300

    🐛 fix: Print push message

--- commands/p.py
+++ commands/p.py
@@ -8,10 +8,10 @@ def p(branch):
     """Push into your GitHub"""
     cmd = ['git', 'push', 'origin', branch]
     try:
-        result = subprocess.run(cmd, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(result.stdout)
+        subprocess.run(cmd, check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        click.echo(f"✅ Pushed into {branch}:")
     except subprocess.CalledProcessError as e:
-        print("Erro:", e.stderr)
+        click.echo(f"❌ Error executing git: {e.stderr}")
 
 if __name__ == "__main__":
     p()
